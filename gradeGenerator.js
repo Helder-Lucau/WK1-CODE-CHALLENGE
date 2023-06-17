cb75ea584c4f9eea2cd4e4be1fd638ce909281b4
Helder <helderlucau@gmail.com>
2023-06-18 01:27:13 +0300

    Completed

--- gradeGenerator.js
+++ gradeGenerator.js
@@ -1,12 +1,18 @@
-//CHALLENGE 1: Student Grade Generator
+/*Student Grade Generator:
+The code below will get input from the user and display the grade they scored.
+The input should be between 0 and 100. The output should be in reference 
+to the below grading system:
+A > 79, B - 60 to 79, C -  59 to 49, D - 40 to 49, E - less 40.
+*/
+
 
 // prompt the user to enter student marks
 const mark = prompt("Enter student mark in range of 0-100");
 let grade;
 
-// declaring a function name studentGrade
+// declaring function studentGrade
 function studentGrade(){
-// check for condition 
+// check for condition
     if (mark >= 0 && mark <= 100) {
         if (mark >= 79) {
             grade = "A";
@@ -19,9 +25,9 @@ function studentGrade(){
         } else {
             grade = "E";
         }
-        window.alert(`You scored ${grade}`); //display message of grade scored using interpolation 
+        window.alert(`You scored ${grade}`); //display message of grade scored  
     } else {
-        window.alert("Invalid marks[range 0 - 100]"); //display message if the marks dont satify the condition
+        window.alert("Invalid marks[range 0 - 100]"); //display message if condition is false
     }
 }
 studentGrade(); //calling the function
