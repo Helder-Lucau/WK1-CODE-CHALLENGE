813d036187e4d5d33c3408b4b488aa2992b64413
Helder <helderlucau@gmail.com>
2023-06-18 01:37:04 +0300

    changed names of files

--- gradeGenerator.js
+++ gradeGenerator.js
@@ -5,7 +5,7 @@ to the below grading system:
 A > 79, B - 60 to 79, C -  59 to 49, D - 40 to 49, E - less 40.
 */
 
-
+git
 // prompt the user to enter student marks
 const mark = prompt("Enter student mark in range of 0-100");
 let grade;
