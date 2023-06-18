c3b06cd19eb6c4083b1f15d816c15e8d619f7fb0
Helder <helderlucau@gmail.com>
2023-06-18 19:56:58 +0300

    Completed

--- speedDetector.js
+++ speedDetector.js
@@ -1,22 +1,28 @@
 /* Speed Detector
+A program that gets speed input from user and checks the following conditions:
+If speed is less than 70 should print "Ok".
+For every 5km/s above the speed limit it should give the driver one demerit point and print the total number of demerit points.
+If the driver gets more than 12 points, should print "License suspended".
 */
 
 //prompts the user input speed of a car
 const speed = prompt("Enter speed of the car");
 
-const speedLimit = 70;
-const points = 5;
-
-// Declared function to check if the speed is less than 70 
+//Declared function to check if the speed is less than 70 
 function speedLimitDetector(){
+    const speedLimit = 70; //variable storing the speed limit
+    const points = 5; //variable storing the initial demeri point
     if (speed < speedLimit) {
         window.alert('Ok');
-    } else if (speed >= speedLimit){
-       demeritPoints = (speed-speedLimit)/points; 
-       window.alert("You got"+" "+demeritPoints+" "+"Points");
-    } 
-    if (demeritPoints > 12 ) {
-    window.alert("License suspended");
+    }
+    else if (speed >= speedLimit){
+       const demeritPoints = (speed-speedLimit)/points; 
+       window.alert("Points:"+" "+demeritPoints); 
+    if (demeritPoints > 12) {   
+    window.alert("License suspended"); // output message if the driver gets more than 12 points
+        }
     }
 }
-speedLimitDetector();
+speedLimitDetector(); //calling the function declared above
+
+
